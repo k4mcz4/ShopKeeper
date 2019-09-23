f8875bcbf592fadea37b709daae35f186235e8ca
Kamil Czarniecki <kamil.czarniecki@peoplevox.com>
2019-09-23 17:25:39 +0200

    Few test changes

--- app/src/main/java/com/czarkam/android/shopkeeper/MainActivity.kt
+++ app/src/main/java/com/czarkam/android/shopkeeper/MainActivity.kt
@@ -2,8 +2,10 @@ package com.czarkam.android.shopkeeper
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
